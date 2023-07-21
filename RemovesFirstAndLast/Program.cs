7d067209732304f61c060b9b933ff9ca11b5fb8d
M.Haris Khan <hkhan@cedrus.com>
2023-07-21 12:35:42 +0500

    Feat;RemoveFirstAndLastCharacters

---
+++ RemovesFirstAndLast/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+public class Program
+{
+    public static string RemoveFirstAndLastCharacters(string inputString)
+    {
+        int length = inputString.Length;
+
+        if (length <= 2)
+            return "";
+
+        string result = "";
+        for (int i = 1; i < length - 1; i++)
+        {
+            result += inputString[i];
+        }
+
+        return result;
+    }
+    public static void Main()
+    {
+        String S = "HelloWorld";
+        Console.WriteLine(RemoveFirstAndLastCharacters(S));
+        Console.ReadLine();
+    }
+}
